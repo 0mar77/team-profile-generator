d86be34f59ca7117ea8fa24f25c9cd37c1ffb759
0mar77 <luisomar7411@gmail.com>
2023-05-12 11:17:57 -0600

    Edited readme file and added comments

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,8 +1,12 @@
+// Imports Employee class
 const Employee = require('../lib/Employee');
 
+// Tests that the inputs matches the outputs
 describe('New employee', () => {
     test('Should return employee name', () => {
+        // Creates a new employee with the following inputs
         const newEmployee = new Employee('John', 1, '123@123.com');
+        // Shows expected results
         expect(newEmployee.getName()).toEqual('John');
         expect(newEmployee.getId()).toEqual(1);
         expect(newEmployee.getEmail()).toEqual('123@123.com');
